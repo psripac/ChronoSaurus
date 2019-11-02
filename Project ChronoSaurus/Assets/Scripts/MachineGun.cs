42ccc419e9afebcf143f3b8f4b523cb8b283eb45
keyluck <38598401+keyluck@users.noreply.github.com>
2019-11-02 16:54:38 -0700

    Weapons update
    Added:
    - WeaponSelecter
    Cycles through different weapons using mouse wheel or number keys
    
    - Pistol
     - Machine Gun

---
+++ Project ChronoSaurus/Assets/Scripts/MachineGun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MachineGun : MonoBehaviour
+{
+    public float damage = 25f;
+    public float range = 200f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+
+
+
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+
+}
---
+++ Project ChronoSaurus/Assets/Scripts/ThirdPController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPController : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    public float jumpHeight;
+    public LayerMask ground;
+    public Transform feet;
+    //public AudioSource audio;
+
+
+    private Vector3 direction;// = new Vector3(0, 0, 1);
+
+    private Vector3 walkingVelocity;
+    private Vector3 fallingVelocity;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10.0f;
+        direction = Vector3.zero;
+        walkingVelocity = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+        gravity = 9.8f;
+        jumpHeight = 10.0f;
+        fallingVelocity = Vector3.zero;
+        //audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        
+        direction = direction.normalized;
+
+        walkingVelocity = direction * speed;
+        controller.Move(walkingVelocity * Time.deltaTime);
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            Debug.Log(direction);
+        }
+
+        /*bool isGrounded = Physics.CheckSphere(feet.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+
+        if (isGrounded)
+            fallingVelocity.y = 0f;
+        else*/
+            fallingVelocity.y -= gravity * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump"))// && isGrounded)
+        {
+            //audio.Play();
+            fallingVelocity.y = Mathf.Sqrt(gravity * jumpHeight);
+        }
+        controller.Move(fallingVelocity * Time.deltaTime);
+    }
+}
+
+
+
+
+
+
+
+   
