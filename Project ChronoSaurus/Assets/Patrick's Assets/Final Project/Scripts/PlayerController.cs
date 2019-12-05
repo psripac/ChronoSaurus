3866a64650b446f0897fb933a2da8f3f63133ec4
psripac <psriprac@yahoo.com>
2019-12-05 01:17:29 -0800

    Player Collision/Anim
    Updated controller scripts and added camera look and follow

---
+++ Project ChronoSaurus/Assets/Patrick's Assets/Final Project/Scripts/PlayerController.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    private Animator animator;
+    public float speed = 4f;
+
+    Vector3 lookPos;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(horizontal, 0, vertical);
+
+        animCharacter(movement, lookDir);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        rigidBody.velocity = direction*speed;
+    }
+
+    void animCharacter(Vector3 moveTo, Vector3 lookTo)
+    {
+        Quaternion lookQuaternion = (Quaternion.LookRotation(lookTo, Vector3.up));
+        float lookInQuaternions = lookQuaternion.y;
+
+        if (lookInQuaternions >= 0.9) //looking down
+        {
+            if (moveTo.z < 0) //moving down
+            {
+                animator.SetFloat("walkForward", (-moveTo.z));
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z > 0) //moving up
+            {
+                animator.SetFloat("walkBackward", moveTo.z);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.x < 0) //moving left
+            {
+                animator.SetFloat("strafeRight", (-moveTo.x));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+            }
+            if (moveTo.x > 0) //moving right
+            {
+                animator.SetFloat("strafeLeft", moveTo.x);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+        }
+        else if (lookInQuaternions < (-0.3) && lookInQuaternions >= (-0.9)) //looking left
+        {
+            if (moveTo.x > 0) //moving right
+            {
+                animator.SetFloat("walkBackward", moveTo.x);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.x < 0) //moving left
+            {
+                animator.SetFloat("walkForward", (-moveTo.x));
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z > 0) //moving up
+            {
+                animator.SetFloat("strafeRight", moveTo.z);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+            }
+            if (moveTo.z < 0) //moving down
+            {
+                animator.SetFloat("strafeLeft", (-moveTo.z));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+        }
+        else if (lookInQuaternions >= (-0.3) && lookInQuaternions <= 0.3) //looking up
+        {
+            if (moveTo.x > 0) //moving right
+            {
+                animator.SetFloat("strafeRight", moveTo.x);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+            }
+            if (moveTo.x < 0) //moving left
+            {
+                animator.SetFloat("strafeLeft", (-moveTo.x));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z > 0) //moving up
+            {
+                animator.SetFloat("walkForward", moveTo.z);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z < 0) //moving down
+            {
+                animator.SetFloat("walkBackward", (-moveTo.z));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+        }
+        else if (lookInQuaternions > 0.3 && lookInQuaternions < 0.9) //looking right
+        {
+            if (moveTo.x > 0) //moving right
+            {
+                animator.SetFloat("walkForward", moveTo.x);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.x < 0) //moving left
+            {
+                animator.SetFloat("walkBackward", (-moveTo.x));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("strafeLeft", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z > 0) //moving up
+            {
+                animator.SetFloat("strafeLeft", moveTo.z);
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeRight", 0);
+            }
+            if (moveTo.z < 0) //moving down
+            {
+                animator.SetFloat("strafeRight", (-moveTo.z));
+                animator.SetFloat("walkForward", 0);
+                animator.SetFloat("walkBackward", 0);
+                animator.SetFloat("strafeLeft", 0);
+            }
+        }
+        else
+        {
+            animator.SetFloat("walkForward", 0);
+            animator.SetFloat("walkBackward", 0);
+            animator.SetFloat("strafeLeft", 0);
+            animator.SetFloat("strafeRight", 0);
+        }
+
+    }
+}
