5ef50e10f36bdd553bd39ecaa0b758da4c49e25c
psripac <psriprac@yahoo.com>
2019-12-06 01:14:19 -0800

    Update to Weapongs
    Added automatic fire/fire rates

--- Project ChronoSaurus/Assets/Patrick's Assets/Final Project/Scripts/GunAuto.cs
+++ Project ChronoSaurus/Assets/Patrick's Assets/Final Project/Scripts/GunAuto.cs
@@ -4,6 +4,7 @@ public class GunAuto : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
 
     public ParticleSystem MuzzleFlash;
     public ParticleSystem cartridgeEjection;
@@ -15,11 +16,14 @@ public class GunAuto : MonoBehaviour
 
     public float BULLET_BASE_SPEED = 1.0f;
 
+    private float nexTimeToFire = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        while (Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time >= nexTimeToFire)
         {
+            nexTimeToFire = Time.time + 1f / fireRate;
             Shoot();
             Debug.Log("Gun Firing...");
         }
--- Project ChronoSaurus/Assets/Patrick's Assets/Final Project/Scripts/GunSemiAuto.cs
+++ Project ChronoSaurus/Assets/Patrick's Assets/Final Project/Scripts/GunSemiAuto.cs
@@ -4,6 +4,7 @@ public class GunSemiAuto : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
 
     public ParticleSystem MuzzleFlash;
     public ParticleSystem cartridgeEjection;
@@ -11,16 +12,20 @@ public class GunSemiAuto : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject bulletSpawn;
     GameObject pc;
+    WaitForSecondsRealtime wait;
 
     public float BULLET_BASE_SPEED = 1.0f;
+
+    private float nexTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nexTimeToFire)
         {
+            nexTimeToFire = Time.time + 1f / fireRate;
             Shoot();
-            Debug.Log("Gun Fired...");
+            Debug.Log("Gun Firing...");
         }
 
     }
